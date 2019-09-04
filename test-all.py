da46461d9dba2d2f122f41f12b4f964acdf090e5
Thomas Kent <tom@teeks99.com>
2019-09-03 20:45:56 -0500

    Working with command line options

--- test-all.py
+++ test-all.py
@@ -1,5 +1,6 @@
 import subprocess
 import string
+import sys
 
 dev_version = "10"
 versions=["8", "9", "10"]
@@ -47,44 +48,59 @@ def make_dockerfile(version, distro, release):
     with open("Dockerfile", "w") as f:
         f.write(out)
 
-def main():
+def make_config(version, distro, release):
+    name = "{}-{}".format(release, version)
+    print("=" * 79)
+    print("Starting: " + name + "\n")
+
+    try:
+
+        make_dockerfile(version, distro, release)
+
+        cmd = "docker build -t {tag}:{release}-{version} ."
+        cmd = cmd.format(tag=tag, release=release, version=version)
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
+    except Exception as e:
+        return "Build Failed: {}---{}".format(name, e)
+
+    try:
+        cmd = "docker run --rm -i -t {tag}:{release}-{version}"
+        cmd += " clang++-{version} --version"
+        cmd = cmd.format(tag=tag, release=release, version=version)
+        print(cmd)
+        ver_str = subprocess.check_output(
+            cmd, shell=True, stderr=subprocess.STDOUT).decode()
+
+        if not ver_str.startswith("clang version {}".format(version)):
+            return "Incorrect version: {}---{}".format(name, ver_str)
+
+    except Exception as e:
+        return "Run Failed: {}---{}".format(name, e)
+
+    return ""
+
+def all():
     errors = []
     for version in versions:
         for config in configs:
             distro = config[0]
             release = config[1]
 
-            name = "{}-{}".format(release, version)
-            print("=" * 79)
-            print("Starting: " + name + "\n")
+            error = make_config(version, distro, release)
+            if error:
+                print(error)
+                errors.append(error)
 
-            try:
-
-                make_dockerfile(version, distro, release)
-
-                cmd = "docker build -t {tag}:{release}-{version} ."
-                cmd = cmd.format(tag=tag, release=release, version=version)
-                print(cmd)
-                subprocess.check_call(cmd, shell=True)
-            except Exception as e:
-                print(e)
-                errors.append("Build Failed: {}---{}".format(name, e))
-
-            try:
-                cmd = "docker run --rm -i -t {tag}:{release}-{version}"
-                cmd += " clang++-{version} --version"
-                cmd = cmd.format(tag=tag, release=release, version=version)
-                print(cmd)
-                ver_str = subprocess.check_output(
-                    cmd, shell=True, stderr=subprocess.STDOUT).decode()
-
-                if not ver_str.startswith("clang version {}".format(version)):
-                    print(ver_str)
-                    errors.append("Incorrect version: {}---{}".format(
-                        name, ver_str))
-            except Exception as e:
-                print(e)
-                errors.append("Run Failed: {}---{}".format(name, e))
+    print("Errors encountered")
+    for error in errors:
+        print(error)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 4:
+        distro = sys.argv[1]
+        release = sys.argv[2]
+        version = sys.argv[3]
+        print(make_config(version, distro, release))
+    else:
+        all()
